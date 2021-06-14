1bec422aa8c14ef4bb972cdec9ace454b525ac88
Vladyslav Okopnyy <harmfulcati@gmail.com>
2021-06-14 18:21:06 +0300

    Modifica

--- laravel-main/resources/views/pages/index.blade.php
+++ laravel-main/resources/views/pages/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <main>
-        <h2>
-            PROVA
-        </h2>
+  <main>
+    <h2>
+      PROVA
+    </h2>
 
-        <ul>
-            <li>
-                Martino test
-                Vladus test
-                Giovanni test
-            </li>
+      <ul>
+
+        <li>
+          Martino test
+          Vladus test
+          Giovanni test
+        </li>
+
         </ul>
     </main>
 @endsection
